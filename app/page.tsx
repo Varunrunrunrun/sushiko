e9170f859743962abef42a8cefdb15690e7326d7
varun.nayak <varunrunrunrun@gmail.com>
2024-08-14 18:01:02 +0530

    responsive fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import NavBar from "@/components/reusable/NavBar";
 export default function Home() {
   return (
     <main className="flex h-screen  gap-4 sm:p-4 p-0 bg-primaryCol text-secondaryCol">
-      <div className="flex-1 h-full relative bg-landing-hero md:bg-left bg-center bg-cover sm:rounded-[20px] rounded-none flex flex-col justify-between items-start sm:p-8 p-4">
-        <NavBar />
-        <div className="text-5xl md:text-7xl lg:text-9xl w-full h-auto">
-          <div>SUSHI</div>
-          <div>SENSTATION</div>
+      <div className="flex-1 h-full relative ">
+        <div className="w-full h-full p-4 bg-landing-hero md:bg-left bg-center bg-cover sm:rounded-[20px] rounded-none flex flex-col justify-between items-start ">
+          <NavBar />
+          <div className="text-5xl md:text-7xl lg:text-9xl w-full h-auto">
+            <div>SUSHI</div>
+            <div>SENSTATION</div>
+          </div>
         </div>
       </div>
       <div className="min-w-60 w-1/5 h-full md:flex hidden flex-col justify-between items-center gap-4">
